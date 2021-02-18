df1d5b2d942af2c32c144d380517a5efad4d2a8c
demyansson <idcadifi@gmail.com>
2021-02-18 20:55:16 +0200

    setup config

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 import {newsRouter} from "./news/news.router.js";
+import * as path from "path";
 
 export class App {
-  port = process.env.PORT || 3000;
-
   init() {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    dotenv.config({
+      path: path.join(__dirname, '..', '.env')
+    })
+
+    this.port = process.env.PORT || 3000;
+
     this.server = express();
 
     this.server.use('/news', newsRouter);
